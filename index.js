83d4e6ee507a5012695dd93b696c0124ddaa4a96
Sarah Loutrel <s.loutrel@gmail.com>
2021-09-06 14:20:14 -0700

    add new port

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ app.use((err, req, res, next) => {
   if (!err.message) err.message = 'Something went wrong!';
   res.status(statusCode).render('error', { err });
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`serving on port ${port}`);
 });
